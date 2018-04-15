4a4f6dc519c8d5ad5c7702e555f0adcf150a56e8
camlloyd2 <cvl8ec@irginia.edu>
2018-04-15 18:01:37 -0400

    add link to admin from about.jsp, load site statistics in admin.jsp

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -6,7 +6,17 @@
 
 package codeu.controller;
 
+import codeu.model.data.Conversation;
+import codeu.model.data.Message;
+import codeu.model.data.User;
+import codeu.model.store.basic.ConversationStore;
+import codeu.model.store.basic.MessageStore;
+import codeu.model.store.basic.UserStore;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,9 +28,41 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AdminServlet extends HttpServlet {
 
+// store classes giving access to users, conversations, and messages
+ private UserStore userStore; 
+  private ConversationStore conversationStore;
+  private MessageStore messageStore;
+
+ @Override
+ public void init() throws ServletException {
+   super.init();
+  setConversationStore(ConversationStore.getInstance());
+  setMessageStore(MessageStore.getInstance());
+  setUserStore(UserStore.getInstance());
+ }
+
+
+  void setConversationStore(ConversationStore conversationStore) {
+    this.conversationStore = conversationStore;
+  }
+
+  void setMessageStore(MessageStore messageStore) {
+    this.messageStore = messageStore;
+  }
+
+  void setUserStore(UserStore userStore) {
+   this.userStore = userStore;
+ }
+
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException {
+      List<String> stats = new ArrayList<>();
+      stats.addAll(userStore.getUserStats());
+      stats.addAll(conversationStore.getConversationStats());
+      stats.addAll(messageStore.getMessageStats());
+      request.setAttribute("statistics", stats);
       request.getRequestDispatcher("/WEB-INF/view/admin.jsp").forward(request, response);
   }
 }
--- src/main/webapp/WEB-INF/view/admin.jsp
+++ src/main/webapp/WEB-INF/view/admin.jsp
@@ -1,3 +1,11 @@
+<%@ page import="java.util.List" %>
+<%@ page import="codeu.model.data.Conversation" %>
+<%@ page import="codeu.model.data.Message" %>
+<%@ page import="codeu.model.store.basic.UserStore" %>
+<%
+List<String> statistics = (List<String>) request.getAttribute("statistics");
+%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,5 +14,16 @@
 <body>
  <h1>Admin</h1>
  <p>Temporary jsp. Bigger and better coming soon!</p>
+ <div id-"stats">
+     <ul>
+  <%
+        for (String stat : statistics) {
+      %>
+      <li><%= stat %></li>
+      <%
+     }
+      %>
+     </ul>
+ </div>
 </body>
 </html>
