5ce696d2ad3a43ab75e4c6e341244106af2c7136
amuhammad135 <am4411@columbia.edu>
2018-05-02 16:27:17 -0400

    added necessary imports for testing

--- src/test/java/codeu/controller/LogoutServletTest.java
+++ src/test/java/codeu/controller/LogoutServletTest.java
@@ -5,6 +5,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.mockito.Mockito;
+import org.junit.Before;
+import org.junit.Test;
 
 
 public class LogoutServletTest {
