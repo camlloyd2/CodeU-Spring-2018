f99385e6af42d9dbf8f563b651adc3c5bd669028
amuhammad135 <am4411@columbia.edu>
2018-05-02 16:23:36 -0400

    added logout feature for homepage

---
+++ src/test/java/codeu/controller/LogoutServletTest.java
@@ -0,0 +1,30 @@
+package codeu.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class LogoutServletTest {
+
+  private LogoutServlet logoutServlet;
+  private HttpServletRequest mockRequest;
+  private HttpServletResponse mockResponse;
+
+  @Before
+    public void setup() throws IOException {
+      logoutServlet = new LogoutServlet();
+      mockRequest = Mockito.mock(HttpServletRequest.class);
+      mockResponse = Mockito.mock(HttpServletResponse.class);
+    }
+
+  @Test
+   public void testDoGet() throws IOException, ServletException {
+     HttpSession mockSession = Mockito.mock(HttpSession.class);
+
+     logoutServlet.doGet(mockRequest, mockResponse);
+     Mockito.verify(mockResponse).sendRedirect("/login");
+   }
+ }
